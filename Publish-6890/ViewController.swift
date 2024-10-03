80016a421009f0c316444b1a7488e78623d75c65
coderpath <palashiosdeveloper@gmail.com>
2024-10-03 08:48:44 +0530

    Initial Commit

---
+++ Publish-6890/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Publish-6890
+//
+//  Created by CoderPath on 03/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dataShowText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        dataShowText.text = "lklkl"
+    }
+
+
+}
+
